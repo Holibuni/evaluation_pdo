c59614887fa45d3ddf4e069ddee9843437fb4d40
Cloé <cloeadeline10@gmail.com>
2025-07-16 11:18:08 +0200

    Emprunts_php

--- Eval 2/index.php
+++ Eval 2/index.php
@@ -19,12 +19,39 @@
         $req->execute();
         $emprunts = $req->fetchAll(PDO::FETCH_ASSOC);
 
-        foreach ($emprunts as $emprunt) {
-            echo "<li>";
-            echo $emprunt['nom_employe'];
-            echo "</li>";
-        }
+        // foreach ($emprunts as $emprunt) {
+        //     echo "<li>";
+        //     echo $emprunt['nom_employe'];
+        //     echo ", ";
+        //     echo $emprunt['materiel'];
+        //     echo ", ";
+        //     echo $emprunt['date_emprunt'];
+        //     echo ", ";
+        //     echo $emprunt['commentaire'];
+        //     echo "</li>";
+        // }
      ?>
+         <table border="1">
+        <tr>
+            <th>Nom de l’employé</th>
+            <th>Matériel emprunté</th>
+            <th>Date d’emprunt</th>
+            <th>Commentaire</th>
+        </tr>
+        <?php foreach ($emprunts as $emprunt): ?>
+        <tr>
+            <td><?= htmlspecialchars($emprunt['nom_employe']) ?></td>
+            <td><?= htmlspecialchars($emprunt['materiel']) ?></td>
+            <td>
+                <?php
+                    $date = new DateTime($emprunt['date_emprunt']);
+                    echo $date->format('d/m/Y');
+                ?>
+            </td>
+            <td><?= !empty($emprunt['commentaire']) ? htmlspecialchars($emprunt['commentaire']) : '-' ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
      <a class="entry" href="./add.php">→ Voir tous les emprunts</a>
 </body>
 </html>
